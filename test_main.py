2e85af7067e42cd333e6fec888e075d3c2ffa49b
KirilZlatevR <79504993+KirilZlatevR@users.noreply.github.com>
2024-02-16 09:48:24 +0200

    test

--- test_main.py
+++ test_main.py
@@ -13,3 +13,14 @@ def test_get_access_token():
 
     print(access_token)
 
+def test_get_access_token2():
+    data = {"returnUrl": 'home', "email": 'sisautomationtest@gustr.com', "password": 'Qwerty123'}  
+    response = requests.post("https://api.dev.sis.amexis.net/api/account/login", json=data)
+    assert response.status_code == 200
+    response_to_json = response.json()
+    bearer_token = BearerTokenResponse.Token(response_to_json['accessToken'], response_to_json['refreshToken'])
+
+    access_token = bearer_token.accessToken
+
+    print(access_token)
+
